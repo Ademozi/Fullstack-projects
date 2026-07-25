61fa89477d0d88fdbd49c9ff3738e1aac7364691
Ademozi <ademoviadem@gmail.com>
2026-07-25 16:15:18 +0100

    Create the register function

---
+++ WeatherSphere/backend/controllers/authControllers.js
@@ -0,0 +1,53 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        if (!name || !email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required."
+            });
+        }
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(409).json({
+                message: "Email already exists"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        await newUser.save();
+        res.status(201).json({
+            message: "User registered successfully",
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
+
+    }
+    catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        });
+    }
+};
+
+module.exports = {
+    register
+};
--- WeatherSphere/backend/server.js
+++ WeatherSphere/backend/server.js
@@ -2,12 +2,13 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 connectDB();
+app.use("/api/auth", authRoutes);
 
 app.use(cors());
 app.use(express.json());
